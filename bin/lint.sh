40031a27875d800f17a8b8e7899e8304a16256c3
Noel De Martin <noel@noeldemartin.com>
2021-12-11 09:57:01 +0100

    Add lint logs

--- bin/lint.sh
+++ bin/lint.sh
@@ -3,14 +3,17 @@
 if [[ -f '.eslintrc.js' ]]; then
     for folder in $@
     do
+        echo "Running eslint for $folder..."
         npx eslint $folder
     done
 fi
 
 if [[ -f 'tsconfig.json' ]]; then
+    echo "Running tsc..."
     npx tsc --noEmit
 fi
 
 if [[ -f 'node_modules/vue-tsc/package.json' ]]; then
+    echo "Running vue-tsc..."
     npx vue-tsc --noEmit
 fi
