f891bcbf278e01c7429bb641d0df38556b4ac151
Noel De Martin <noel@noeldemartin.com>
2025-03-19 19:59:22 +0100

    Restore deploy-app.sh script

---
+++ bin/deploy-app.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Optional domain
+domain=$1
+
+# Abort on errors
+set -e
+
+# Build
+rm -rf dist
+npm run build
+
+# Navigate into the build output directory
+cd dist
+
+# Prepare github pages
+if [[ -n $domain ]]; then
+    echo "$domain" > CNAME
+fi
+
+touch .nojekyll
+
+# Publish
+git init
+git config user.name 'github-actions[bot]'
+git config user.email 'github-actions[bot]@users.noreply.github.com'
+git checkout -b gh-pages
+git add -A
+git commit -m 'deploy'
+git push -f https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git gh-pages
+
+cd -
