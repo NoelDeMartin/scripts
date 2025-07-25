4f5c8122150097c05740e0252c9a77b573ddc1fd
Noel De Martin <noel@noeldemartin.com>
2025-07-25 18:31:47 +0200

    Add --exclude-entrypoints to verify command

--- bin/verify.sh
+++ bin/verify.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
 
 set -e
+
+# Parse arguments
+ATTW_ARGS=""
+i=1
+while [ $i -le $# ]; do
+    arg="${!i}"
+
+    if [ "$arg" == "--exclude-entrypoints" ]; then
+        if [ $((i + 1)) -le $# ]; then
+            next_arg=$(eval echo \$$((i + 1)))
+            ATTW_ARGS="$ATTW_ARGS --exclude-entrypoints $next_arg"
+            i=$((i + 1))
+        fi
+    fi
+
+    i=$((i + 1))
+done
+
+# Run checks
 npm pack
 npx publint *.tgz
-npx attw *.tgz --profile esm-only
+npx attw *.tgz --profile esm-only $ATTW_ARGS
